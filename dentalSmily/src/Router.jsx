5c61a4333776044df7476df693fe7149d696e9dd
alvarito101093 <alvaro101093@gmail.com>
2023-03-13 17:51:39 +0100

    feat: Created Router.jsx and NavBar-Bootstrap initial

---
+++ dentalSmily/src/Router.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { About } from './layout/about/About';
+import { Home } from './layout/home/Home';
+import { Login } from './layout/login/Login';
+import { Register } from './layout/register/Register';
+import { Services } from './layout/services/Services';
+
+
+export const Router = () => {
+  return (
+    <>
+    <hr />
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/about' element={<About/>} />
+    </Routes>
+    </>
+  )
+}
--- dentalSmily/src/components/NavBar.jsx
+++ dentalSmily/src/components/NavBar.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css'
+import { Link } from 'react-router-dom';
 
-export const NavBar = () => {
+function BasicExample() {
   return (
-    <div>NavBar</div>
-  )
+    <div className='navbarstyle'>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Welcome To dentalSmily</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            <NavDropdown title="Services" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/login"> Login </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/register"> Register </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  );
 }
+
+export default BasicExample;
