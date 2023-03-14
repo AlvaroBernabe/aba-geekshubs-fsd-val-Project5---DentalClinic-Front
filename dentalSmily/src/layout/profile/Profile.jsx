0575fde7c765a770a42f628532681f999ac2caae
alvarito101093 <alvaro101093@gmail.com>
2023-03-14 15:08:30 +0100

    feat:Form Profile is kind of finished

--- dentalSmily/src/layout/profile/Profile.jsx
+++ dentalSmily/src/layout/profile/Profile.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import './Profile.css'
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import NavBar from '../../components/NavBar';
+import './Profile.css';
 
-export const Profile = () => {
-  return (
-    <>
+export function Profile() {
+    return (
+        <>
         <NavBar />
         <hr />
-        <div>You are in Profile</div>
-    </>
-  )
+        <div style={{ display: 'block', 
+                    width: 700, 
+                    padding: 30 }}>
+        <h4>React-Bootstrap Form Component</h4>
+        <Form>
+            <Form.Group>
+                <Form.Label>Full Name:</Form.Label>
+                <Form.Control type="text" placeholder="Name input" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>DNI:</Form.Label>
+                <Form.Control type="text" placeholder="DNI input" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Phone:</Form.Label>
+                <Form.Control type="tel" placeholder="Phone input" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Email:</Form.Label>
+                <Form.Control type="email" placeholder="Email input" />
+            </Form.Group>
+            <br />
+            <Form.Select aria-label="Payment Method">
+                <option>Default Payment Method</option>
+                <option value="1">Card</option>
+                <option value="2">Cash</option>
+                <option value="3">Paypal</option>
+            </Form.Select>
+            <br />
+            <div className='botonModificar'>
+            <Button variant="primary" type="submit">
+                Modificar Datos
+            </Button>
+            </div>
+        </Form>
+        </div>
+        </>
+    );
 }
