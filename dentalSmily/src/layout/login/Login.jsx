5586b4634ef499b653dfc5a40b6ca0f289971ff1
alvarito101093 <alvaro101093@gmail.com>
2023-03-14 18:48:34 +0100

    feat: Login form have value

--- dentalSmily/src/layout/login/Login.jsx
+++ dentalSmily/src/layout/login/Login.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import NavBar from '../../components/NavBar';
 import './Login.css';
 
 export function Login() {
+    let user = {
+        email: '',
+        password: ''
+    };
+    const [valor, setValor] = useState(user);
+    const { email, password} = valor;
+    const newValue = ({target}) => {
+        console.log(target);
+        const {name, value} = target;
+        setValor({...valor,[name]:value});
+    }
     return (
         <>
         <NavBar />
@@ -14,11 +25,11 @@ export function Login() {
                 <Form>
                     <Form.Group>
                         <Form.Label>Enter your email:</Form.Label>
-                        <Form.Control type="email" placeholder="Enter your email address" />
+                        <Form.Control type="email"  name="email" placeholder="Enter your email address"  value={email} onChange={newValue}/>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Enter your password:</Form.Label>
-                        <Form.Control type="password" placeholder="Enter your password" />
+                        <Form.Control type="password" name="password" placeholder="Enter your password" value={password} onChange={newValue} />
                     </Form.Group>
                     <br />
                     <div className='botones'>
