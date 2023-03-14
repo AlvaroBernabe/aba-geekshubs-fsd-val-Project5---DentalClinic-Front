43ebcf340dda15d2c520affe566facb48293fa0e
alvarito101093 <alvaro101093@gmail.com>
2023-03-14 16:23:41 +0100

    feat: Login layout resized

--- dentalSmily/src/layout/login/Login.jsx
+++ dentalSmily/src/layout/login/Login.jsx
@@ -9,27 +9,26 @@ export function Login() {
         <>
         <NavBar />
         <hr />
-        <div style={{ display: 'block', 
-                    width: 700, 
-                    padding: 30 }}>
-        <h4>React-Bootstrap Form Component</h4>
-        <Form>
-            <Form.Group>
-                <Form.Label>Enter your email:</Form.Label>
-                <Form.Control type="email" placeholder="Enter your your email address" />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Enter your password:</Form.Label>
-                <Form.Control type="password" placeholder="Enter your password" />
-            </Form.Group>
-            <br />
-            <div className='botones'>
-              <Button variant="primary" className='botonLogin' style={{ width: "5em", height: "1.9em",}}  type="submit">
-                  Login
-              </Button>
-              <Button variant="info" className='botonRestaurar' style={{ width: "8em", height: "4em",}}  >Reset Password</Button>{' '}
+        <div style={{ display: 'block', width: 700, padding: 30 }}>
+            <div className='formulario'>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Enter your email:</Form.Label>
+                        <Form.Control type="email" placeholder="Enter your email address" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Enter your password:</Form.Label>
+                        <Form.Control type="password" placeholder="Enter your password" />
+                    </Form.Group>
+                    <br />
+                    <div className='botones'>
+                        <Button variant="primary" className='botonLogin' style={{ width: "5em", height: "1.9em",}}  type="submit">
+                        Login
+                        </Button>
+                        <Button variant="info" className='botonRestaurar' style={{ width: "8em", height: "4em",}}  >Reset Password</Button>{' '}
+                    </div>
+                </Form>
             </div>
-        </Form>
         </div>
         </>
     );
