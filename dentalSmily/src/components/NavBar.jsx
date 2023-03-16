6830bab73387900fc7af6c45a0a928b2972becb7
alvarito101093 <alvaro101093@gmail.com>
2023-03-16 23:05:35 +0100

    feat: navBar switch element when user login is correct

--- dentalSmily/src/components/NavBar.jsx
+++ dentalSmily/src/components/NavBar.jsx
@@ -1,11 +1,20 @@
+import React, {useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './NavBar.css'
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { userData } from '../layout/login/userSlice';
 
-function NavBar() {
+function NavBar ()  {
+
+  const credencialesRedux = useSelector(userData);
+
+  useEffect(() => {
+    console.log(credencialesRedux);
+  })
   return (
     <div className='navbarstyle'>
     <Navbar bg="light" expand="lg">
@@ -16,11 +25,23 @@ function NavBar() {
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/treatment">Treatment</Nav.Link>
             <Navbar.Brand  className='Logo'>Welcome To dentalSmily</Navbar.Brand>
-            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-            <NavDropdown title="User" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/login"> Login </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/register"> Register </NavDropdown.Item>
-            </NavDropdown>
+            
+
+            {credencialesRedux.credentials.token ? (
+              <>
+                <Nav.Link as={Link} to='/profile'>Profile</Nav.Link>
+                <Nav.Link as={Link} to='/logout'>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+              </>
+              )}
+            {/* <NavDropdown title="User" id="basic-nav-dropdown"> */}
+              {/* <NavDropdown.Item as={Link} to="/login"> Login </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/register"> Register </NavDropdown.Item> */}
+            {/* </NavDropdown> */}
             <Nav.Link as={Link} to="/about">About Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- dentalSmily/src/layout/services/apiCalls.js
+++ dentalSmily/src/layout/services/apiCalls.js
@@ -6,3 +6,19 @@ export const logMe = async (body) => {
     console.log('apiCalls');
     return await axios.post(`${root}/login`, body);
 };
+
+export const getUserData = async () => {
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjgsImVtYWlsIjoiYWx2YXJvZHNhZmQyc2RAZ21haWwuY29tIiwicm9sZUlkIjoxLCJmdWxsTmFtZSI6ImFsdmFyaXRvMWYyNDEwOTMiLCJpYXQiOjE2NzkwMDM5OTcsImV4cCI6MTY3OTAxMTE5N30._3odQb-XWgDAigk_LOpZyBIPYT52TCgkGz270LdkUdw";
+    let config = {        headers: { Authorization: `Bearer ${token}` }    };
+    // let config = {        headers: { Authorization: `Bearer ${props.datosLogin.token}` }    };
+    return await axios.get(`${root}/user`, config)
+}
+
+export const reservations = async (body, token) => {
+    var config = {
+        headers: { 
+        'Authorization': 'Bearer '+ token,  
+        }
+    };
+    return await axios.get(`${route}/api/reservations`, body, config);
+};
