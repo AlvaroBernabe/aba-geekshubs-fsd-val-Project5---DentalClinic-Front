69ea763bf45ed92ccb186404adc05953090a5e2c
alvarito101093 <alvaro101093@gmail.com>
2023-03-18 20:49:45 +0100

    test: added Login Wellcome message and redirect to Home

--- dentalSmily/src/layout/login/Login.jsx
+++ dentalSmily/src/layout/login/Login.jsx
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from "react";
 import "./Login.css";
-import { useDispatch } from "react-redux";
-import { login } from "./userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, userData } from "../userSlice";
 import { decodeToken } from "react-jwt";
 import { InputText } from "../../components/InputText/InputText";
 import { logMe } from "../services/apiCalls";
 import NavBar from "../../components/NavBar";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
-
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+    const navigate = useNavigate();
+    const credentialsRdx = useSelector(userData);
+    const [welcome, setWelcome] = useState("");
 
     const dispatch = useDispatch();
     const [credenciales, setCredenciales] = useState({
@@ -17,69 +20,84 @@ export const Login = () => {
     password: ''
     })
 
+    useEffect(() => {
+        if (credentialsRdx.credentials.token) {
+          //Si No token...home redirect
+          navigate("/");
+        }
+      }, []);
+
     const inputHandler = (e) => {
-
-    setCredenciales((prevState) => ({
-        ...prevState,
-        [e.target.name]: e.target.value,
-      }));
-
+        setCredenciales((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
   }
 
   const checkError = (e) => {
-
   }
 
   const logeame = () => {
     logMe(credenciales)
-        .then(
-            respuesta => {
+        .then( respuesta => {
               let decodificado = decodeToken(respuesta.data)
-              console.log(decodificado)
+              console.log("este es el  token decodificado", decodificado)
                 let datosBackend = {
                     token: respuesta.data,
                     usuario: decodificado
                 };       
-                console.log("Bienvenido" , datosBackend)
+                // ("Bienvenido" JSON.stringify(datosBackend.usuario.fullName) )
+          //      console.log("Bienvenido" , datosBackend.usuario.roleId)                       //Esto me saca el RoleId 
                 // console.log(datosBackend);
                 //Este es el momento en el que guardo en REDUX
                 dispatch(login({credentials: datosBackend}));
-            }
-        )
+                setWelcome(`Bienvenid@ de nuevo ${datosBackend.usuario.fullName}`);
+        //Redirección a Home
+        setTimeout(() => {
+            navigate("/");
+          }, 3000);
+        })
         .catch(error => console.log(error))
-  }
+  };
 
     return (
         <>
         <NavBar />
         <hr />
         <div style={{ display: 'block', width: 700, padding: 30 }}>
-            <div className='formularioLogin'>
-            <Container>
-                <Row className="LoginForm">
-                    <Col lg={6}>
-                        <Form>
-                            <Form.Group>
-                                <Form.Label>Enter your email account:</Form.Label>
-                                <InputText className={"inputLogin"}
-                                type={"email"} name={"email"} placeholder={"email..."} required={true}
-                                changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Enter your password:</Form.Label>
-                                <InputText className={"inputLogin"}
-                                type={"password"} name={"password"} placeholder={""} required={true} 
-                                changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
-                            </Form.Group>
-                            <br />
-                            <Button className="botonLog" variant="primary" 
-                            onClick={()=> logeame()}> Login User
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="loginDesign">
+                {welcome !== "" ? (
+            <div>{welcome}</div>
+        ) : (
+            <div>
+                <Container>
+                    <Row className="LoginForm">
+                        <Col lg={6}>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Label>Enter your email account:</Form.Label>
+                                    <InputText className={"inputLogin"}
+                                    type={"email"} name={"email"} placeholder={"email..."} required={true}
+                                    changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Enter your password:</Form.Label>
+                                    <InputText className={"inputLogin"}
+                                    type={"password"} name={"password"} placeholder={""} required={true} 
+                                    changeFunction={(e) => inputHandler(e)} blurFunction={(e) => checkError(e)} />
+                                </Form.Group>
+                                <br />
+                                <Button className="botonLog" variant="primary" 
+                                onClick={()=> logeame()}> Login User
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
+        )}
+        </div>
+
         </div>
         </>
     );
