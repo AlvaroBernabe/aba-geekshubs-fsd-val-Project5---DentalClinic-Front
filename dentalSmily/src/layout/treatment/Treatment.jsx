0758baf14ed4dbe321b05091a640fd96d751b312
alvarito101093 <alvaro101093@gmail.com>
2023-03-26 23:30:28 +0200

    feat: Treatment Cards ready for text

--- dentalSmily/src/layout/treatment/Treatment.jsx
+++ dentalSmily/src/layout/treatment/Treatment.jsx
@@ -1,13 +1,137 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 import "./Treatment.css";
+import treatment1 from '../../assets/treatment1.png';
+import treatment2 from '../../assets/treatment2.png';
+import treatment3 from '../../assets/treatment3.png';
+import treatment4 from '../../assets/treatment4.png';
+import treatment5 from '../../assets/treatment5.png';
+import treatment6 from '../../assets/treatment6.png';
+import treatment7 from '../../assets/treatment7.png';
+import treatment8 from '../../assets/treatment8.png';
+import { Card, CardGroup, Figure } from "react-bootstrap";
+
 
 export const Treatment = () => {
     return (
         <>
             <NavBar />
             <hr />
-            <div>You are in Treatment </div>
+            <div>Welcome To the treatments Available</div>
+            <div className="main">
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src={treatment1} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={treatment2} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to
+                        additional content.{' '}
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={treatment3} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This card has even longer content than the
+                        first to show that equal height action.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={treatment4} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src={treatment5} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This card has supporting text below as a natural lead-in to
+                        additional content.{' '}
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={treatment6} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This card has even longer content than the
+                        first to show that equal height action.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={treatment7} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This card has even longer content than the
+                        first to show that equal height action.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={treatment8} />
+                    <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in
+                        to additional content. This card has even longer content than the
+                        first to show that equal height action.
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
+        </div>
         </>
     );
 };
